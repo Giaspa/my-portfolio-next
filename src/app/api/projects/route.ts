3259c77f8bc102f7166303213d1dcc789506f1e4
Gianluca Spadazzi <spadazzi.gianluca@gmail.com>
2024-12-02 18:51:29 +0100

    CORS fix

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -2,13 +2,15 @@ import { NextResponse } from "next/server";
 import { time } from "@/types/common";
 import { createClient } from "@/supabase/client";
 
-export async function GET(){
+export async function GET() {
     const supabase = await createClient();
     const { data: projects } = await supabase.from("Project").select();
 
-    return NextResponse.json(projects, {
-        headers: {
-            'Cache-Control': `s-maxage=${time}, stale-while-revalidate=${time}`
-        }
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "Cache-Control": `s-maxage=${time}, stale-while-revalidate=${time}`,
+        "Access-Control-Allow-Origin": `${process.env.NEXT_PUBLIC_BASE_URL}`,
     });
+
+    return NextResponse.json(projects, { headers });
 }
